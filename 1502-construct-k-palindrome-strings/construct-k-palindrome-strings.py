b98f584eefd6c7fbc84435b83c0c1d4235a43d75
Subham Kumar H <119826176+SubhamKumar-Gaurav@users.noreply.github.com>
2025-01-11 11:27:04 +0530

    Time: 54 ms (21.15%) | Memory: 18.3 MB (8.81%) - LeetSync

--- 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.py
+++ 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.py
@@ -2,14 +2,11 @@ class Solution:
     def canConstruct(self, s: str, k: int) -> bool:
         if len(s)<k : 
             return False 
-        d={}
+        freq=[0]*26 
         for i in s : 
-            if i in d : 
-                d[i]+=1 
-            else : 
-                d[i]=1 
+            freq[ord(i)-ord('a')]+=1 
         odd=0 
-        for i in d : 
-            if d[i]%2 : 
+        for i in range(len(freq)) : 
+            if freq[i]%2 : 
                 odd+=1 
-        return odd<=k
+        return odd<=k 
