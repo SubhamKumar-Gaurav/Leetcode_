2da32bafda8279bacc9a9b49ff5a410aa15f6409
Subham Kumar H <119826176+SubhamKumar-Gaurav@users.noreply.github.com>
2025-02-10 09:40:23 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (43.55%) - LeetSync

--- 3447-clear-digits/clear-digits.py
+++ 3447-clear-digits/clear-digits.py
@@ -1,12 +1,9 @@
 class Solution:
     def clearDigits(self, s: str) -> str:
         res=[]
-        del_cnt=0 
-        for i in reversed(range(len(s))) : 
+        for i in range(len(s)) : 
             if s[i].isdigit() : 
-                del_cnt+=1 
-            elif del_cnt : 
-                del_cnt-=1 
+                res.pop()
             else : 
                 res.append(s[i]) 
-        return "".join(res[::-1])
+        return "".join(res)
