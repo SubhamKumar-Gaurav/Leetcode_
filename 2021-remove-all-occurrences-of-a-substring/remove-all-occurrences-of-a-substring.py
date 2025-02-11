4e4f28653f81be0dbdc098093457f09032703fa0
Subham Kumar H <119826176+SubhamKumar-Gaurav@users.noreply.github.com>
2025-02-11 22:32:24 +0530

    Time: 7 ms (38.80%) | Memory: 18 MB (15.89%) - LeetSync

--- 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.py
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.py
@@ -1,8 +1,32 @@
+
 class Solution:
-    def removeOccurrences(self, s: str, part: str) -> str:
-        while True : 
-            idx=s.find(part) 
-            if idx==-1 : 
-                break 
-            s=s[:idx]+s[idx+len(part):]
-        return s
+    def removeOccurrences(self, s: str, part: str) -> str: 
+        newString=part+"#"+s 
+        freqArray=[0 for _ in range(len(newString))] 
+        ans=[] 
+        n=len(part) 
+        length=0 
+        i=1 
+        while i<len(newString) : 
+            if newString[i]==newString[length] : 
+                length+=1 
+                freqArray[i]=length 
+                if i>n : 
+                    ans.append((newString[i], length)) 
+                if length==n : 
+                    for _ in range(n) : 
+                        ans.pop() 
+                    if not ans : 
+                        length=0 
+                    else : 
+                        length=ans[-1][-1] 
+            else : 
+                if length>0 : 
+                    length=freqArray[length-1] 
+                    i-=1 
+                else : 
+                    freqArray[i]=0 
+                    if i>n : 
+                        ans.append((newString[i], 0))
+            i+=1 
+        return "".join(i[0] for i in ans) 
