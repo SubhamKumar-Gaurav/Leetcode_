f5ae471924b571533d3d466e9be213a04643d90c
Subham Kumar H <119826176+SubhamKumar-Gaurav@users.noreply.github.com>
2025-03-31 14:47:45 +0530

    Time: 329 ms (10.33%) | Memory: 33.4 MB (7.44%) - LeetSync

--- 2681-put-marbles-in-bags/put-marbles-in-bags.py
+++ 2681-put-marbles-in-bags/put-marbles-in-bags.py
@@ -1,13 +1,17 @@
-
 class Solution:
     def putMarbles(self, weights: List[int], k: int) -> int:
         if len(weights)==k or k==1 : 
             return 0 
-        arr=[] 
+        min_heap=[]  
+        max_heap=[] 
         for i in range(len(weights)-1) : 
-            arr.append(weights[i]+weights[i+1]) 
-        arr.sort() 
-        i=k-1
-        max_val=sum(arr[-i:])
-        min_val=sum(arr[:i])
+            min_heap.append(weights[i]+weights[i+1]) 
+            max_heap.append(-weights[i]-weights[i+1])
+        heapify(min_heap) 
+        heapify(max_heap)
+        min_val=0 
+        max_val=0  
+        for i in range(k-1) : 
+            min_val+=heappop(min_heap) 
+            max_val+=(-heappop(max_heap))
         return max_val-min_val
