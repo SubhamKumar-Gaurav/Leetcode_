3cffbd6032f093d4d1c2d1a6cfeafbaed4025626
Subham Kumar H <119826176+SubhamKumar-Gaurav@users.noreply.github.com>
2025-04-05 10:14:11 +0530

    Time: 11 ms (76.73%) | Memory: 17.7 MB (83.05%) - LeetSync

---
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.py
@@ -0,0 +1,8 @@
+# Recursive solution : 2^n Time Complexity
+class Solution:
+    def subsetXORSum(self, nums: List[int]) -> int:   
+        def dfs(idx, total) :  
+            if idx==len(nums) : 
+                return total 
+            return dfs(idx+1, total^nums[idx]) + dfs(idx+1, total)
+        return dfs(0,0)
