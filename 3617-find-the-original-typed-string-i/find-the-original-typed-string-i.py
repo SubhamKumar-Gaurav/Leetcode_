117031fe90527356f100b2bdec988961123b8a59
Subham Kumar H <119826176+SubhamKumar-Gaurav@users.noreply.github.com>
2025-07-01 23:12:45 +0530

    Time: 28 ms (97.18%) | Memory: 17.6 MB (72.11%) - LeetSync

---
+++ 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.py
@@ -0,0 +1,7 @@
+class Solution:
+    def possibleStringCount(self, word: str) -> int:  
+        ans=1 
+        for i in range(len(word)-1) : 
+            if word[i]==word[i+1] : 
+                ans+=1 
+        return ans
