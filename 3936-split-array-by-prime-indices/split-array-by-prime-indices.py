885c728e2955435e9c7c4f0c18aff698a5e19755
Subham Kumar H <119826176+SubhamKumar-Gaurav@users.noreply.github.com>
2025-07-20 03:18:07 +0530

    Time: 71 ms (100.00%) | Memory: 34.2 MB (100.00%) - LeetSync

---
+++ 3936-split-array-by-prime-indices/split-array-by-prime-indices.py
@@ -0,0 +1,23 @@
+class Solution:
+    def splitArray(self, nums: List[int]) -> int:
+        n=len(nums) 
+        if n==1 : 
+            return sum(nums)
+            
+        arr=[True]*(n+1) 
+        arr[0]=False 
+        arr[1]=False 
+        i=2 
+        for i in range(2, int(n**0.5)+1) : 
+            if arr[i] : 
+                for j in range(i*i, n+1, i) : 
+                    arr[j]=False                   
+      
+        sum_A=0 
+        sum_B=0
+        for i in range(len(nums)) : 
+            if not arr[i] : 
+                sum_A+=nums[i] 
+            else : 
+                sum_B+=nums[i] 
+        return abs(sum_A-sum_B) 
