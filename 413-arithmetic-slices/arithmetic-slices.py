f9c537e3a50787f54ffbe19c7dbd2d8df5e26cfd
Subham Kumar H <119826176+SubhamKumar-Gaurav@users.noreply.github.com>
2025-08-19 09:45:31 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (78.40%) - LeetSync

--- 413-arithmetic-slices/arithmetic-slices.py
+++ 413-arithmetic-slices/arithmetic-slices.py
@@ -1,13 +1,19 @@
 class Solution:
     def numberOfArithmeticSlices(self, nums: List[int]) -> int: 
-         
+        ## if len<3 --> no subarray will be formed 
         if len(nums)<3 : 
             return 0 
+
+        ## if len==3 : 
+        ## two cases - i) if diff same : return 1 ;
+        ##            ii) else : return 0 
         elif len(nums)==3 : 
             if nums[1]-nums[0] == nums[2]-nums[1] : 
                 return 1 
             else : 
                 return 0 
+        
+        # we iterate over the array until the curr_diff is same (and increase the count too), else we move forward and update the curr_diff 
         else : 
             count=0 
             curr_diff=nums[1]-nums[0] 
