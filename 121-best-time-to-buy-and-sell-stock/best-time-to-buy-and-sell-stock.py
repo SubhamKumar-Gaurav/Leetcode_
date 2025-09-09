8c9945b543eb78c652fb196f08a075e89ff3856f
Subham Kumar H <119826176+SubhamKumar-Gaurav@users.noreply.github.com>
2025-09-10 00:09:50 +0530

    Time: 142 ms (11.56%) | Memory: 26.8 MB (77.99%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,19 +1,8 @@
 class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        n=len(prices)
-        prefixMin=[0]*n 
-        prefixMin[0]=prices[0] 
-        for i in range(1, n) : 
-            prefixMin[i]=min(prices[i], prefixMin[i-1]) 
-        
-        suffixMax=[0]*n 
-        suffixMax[-1]=prices[-1] 
-        for i in range(n-2, -1, -1) : 
-            suffixMax[i]=max(prices[i], suffixMax[i+1]) 
-        
-        ans=0 
-        for i in range(n-1) : 
-            if suffixMax[i+1]>prefixMin[i] : 
-                curr=suffixMax[i+1]-prefixMin[i] 
-                ans=max(ans, curr)
-        return ans
+    def maxProfit(self, prices: List[int]) -> int: 
+        minval=float("inf") 
+        maxval=0 
+        for i in prices :  
+            minval=min(minval, i) 
+            maxval=max(maxval, i-minval) 
+        return maxval
