b4b3a5f374b10851f7a48085fb37c20ad7c7a922
Subham Kumar H <119826176+SubhamKumar-Gaurav@users.noreply.github.com>
2025-09-12 08:51:41 +0530

    Time: 39 ms (36.36%) | Memory: 18 MB (95.15%) - LeetSync

---
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.py
@@ -0,0 +1,9 @@
+class Solution:
+    def doesAliceWin(self, s: str) -> bool: 
+        vowel_count=0 
+        for i in s : 
+            if i in {"a", "e", "i", "o", "u" } : 
+                vowel_count+=1 
+        if vowel_count==0 : 
+            return False 
+        return True 
