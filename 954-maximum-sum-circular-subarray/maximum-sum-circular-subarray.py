6a16d36d212571c2fd07d7e2b270adb8e02a5bca
Subham Kumar H <119826176+SubhamKumar-Gaurav@users.noreply.github.com>
2025-09-15 22:52:40 +0530

    Time: 119 ms (10.07%) | Memory: 21.2 MB (85.18%) - LeetSync

---
+++ 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.py
@@ -0,0 +1,22 @@
+class Solution:
+    def maxSubarraySumCircular(self, nums: List[int]) -> int:
+        def kadanesAlgo(arr) : 
+            n=len(arr) 
+            maxEnding=arr[0] 
+            res=arr[0] 
+            for i in range(1, n) : 
+                maxEnding=max(maxEnding+arr[i], arr[i]) 
+                res=max(res, maxEnding) 
+            return res 
+
+        n=len(nums)
+        maxSubarraySum=kadanesAlgo(nums) 
+        if maxSubarraySum<0 : 
+            return maxSubarraySum 
+        totalArrSum=nums[0]
+        nums[0]=-nums[0] 
+        for i in range(1,n) : 
+            totalArrSum+=nums[i] 
+            nums[i]=-nums[i] 
+        res=totalArrSum+kadanesAlgo(nums) 
+        return max(res, maxSubarraySum)
