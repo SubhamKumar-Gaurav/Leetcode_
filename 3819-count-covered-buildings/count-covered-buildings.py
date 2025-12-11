825d0d75dd51dd78e50be8d85fe6880d6ba20377
Subham Kumar H <119826176+SubhamKumar-Gaurav@users.noreply.github.com>
2025-12-11 19:51:40 +0530

    Time: 914 ms (17.65%) | Memory: 60.3 MB (49.58%) - LeetSync

---
+++ 3819-count-covered-buildings/count-covered-buildings.py
@@ -0,0 +1,29 @@
+from collections import defaultdict 
+class Solution: 
+    def countCoveredBuildings(self, n: int, buildings: List[List[int]]) -> int: 
+        buildings.sort(key=lambda x: (x[0], x[1])) 
+        rows=defaultdict(list) 
+        cols=defaultdict(list) 
+
+        for i, j in buildings :   # buildings are stored in rows and cols 
+            rows[i].append(j) 
+            cols[j].append(i) 
+        
+        # sort the rows and cols list to apply binary search 
+        for i in rows : 
+            rows[i].sort() 
+        
+        for j in cols :
+            cols[j].sort() 
+        
+        ans=0 
+        for i, j in buildings : 
+            rowWise=False 
+            colWise=False 
+            if rows[i][0]!=j and rows[i][-1]!=j : 
+                rowWise=True 
+            if cols[j][0]!=i and cols[j][-1]!=i : 
+                colWise=True 
+            if rowWise and colWise : 
+                ans+=1 
+        return ans 
